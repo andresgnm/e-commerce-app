5537234a973c563efcd1fe637d3fbb3e941cb040
andresgnm <andresgnm13@gmail.com>
2020-10-26 23:49:07 -0600

    add paypal button

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
 
+
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -22,7 +23,9 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona',{
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
-
+app.get('/api/config/paypal', (req,res)=>{
+    res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
